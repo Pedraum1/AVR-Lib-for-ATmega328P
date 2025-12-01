cb4a78a4fba6bd41b289c4f752a9f929ed1568e6
Pedraum1 <pedro.paulo60@aluno.ifce.edu.br>
2025-12-01 12:11:19 -0300

    Criando funções para configurar o timer0 e gerar PWM pro hardware

--- lib/ATmega328/Timers.c
+++ lib/ATmega328/Timers.c
@@ -1,1 +1,44 @@
 #include "Timers.h"
+
+void timer0_set_prescaler(uint8_t prescaler)
+{
+    TCCR0B &= ~(0b00000111);
+
+    if(prescaler > 5) prescaler = 0;
+
+    TCCR0B |= prescaler;
+}
+
+void timer0_set_operation_mode(uint8_t mode)
+{
+    TCCR0A &= ~(0b00000011);
+    if(mode > 3) mode = 0;
+
+    TCCR0A |= mode;
+}
+
+void timer0_config_oc0a(uint8_t output_mode)
+{
+    TCCR0A &= ~(0b11000000);
+    if(output_mode > 3) output_mode = 0;
+
+    TCCR0A |= (output_mode << 6);
+}
+
+void timer0_config_oc0b(uint8_t output_mode)
+{
+    TCCR0A &= ~(0b00110000);
+    if(output_mode > 3) output_mode = 0;
+
+    TCCR0A |= (output_mode << 4);
+}
+
+void timer0_set_ocr0a(uint8_t duty_cicle_8bit)
+{
+    OCR0A = duty_cicle_8bit;
+}
+
+void timer0_set_ocr0b(uint8_t duty_cicle_8bit)
+{
+    OCR0B = duty_cicle_8bit;
+}
--- lib/ATmega328/Timers.h
+++ lib/ATmega328/Timers.h
@@ -2,4 +2,31 @@
 
 #define TIMERS_H
 
+#include "avr/interrupt.h"
+
+#define NORMAL      0
+#define PWM_PHASE   1
+#define CTC         2
+#define FAST_PWM    3
+
+#define PS1     1
+#define PS8     2
+#define PS64    3
+#define PS256   4
+#define PS1024  5
+
+#define NORMAL  0
+#define TOGGLE  1
+#define CLEAR   2
+#define SET     3
+
+void timer0_set_operation_mode(uint8_t mode);
+void timer0_set_prescaler(uint8_t prescaler);
+
+void timer0_config_oc0a(uint8_t output_mode);
+void timer0_config_oc0b(uint8_t output_mode);
+
+void timer0_set_ocr0a(uint8_t duty_cicle_8bit);
+void timer0_set_ocr0b(uint8_t duty_cicle_8bit);
+
 #endif
